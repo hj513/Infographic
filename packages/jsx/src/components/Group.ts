6c52dce2ff967a6d048c76c975467102129f7b45
Aaron <antvaaron@gmail.com>
2025-09-15 15:30:51 +0800

    fix(jsx): x/y attrs of group

--- packages/jsx/src/components/Group.ts
+++ packages/jsx/src/components/Group.ts
@@ -3,7 +3,7 @@ import type { GroupProps, JSXElement } from '../types';
 export function Group(props: GroupProps): JSXElement {
   const { x = 0, y = 0 } = props;
   if (x || y) {
-    props.transform = `translate(${x}, ${y})${props.transform ? ' ' + props.transform : ''}`;
+    props.transform ||= `translate(${x}, ${y})`;
   }
   const node: JSXElement = {
     type: 'g',
